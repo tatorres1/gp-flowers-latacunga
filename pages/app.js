edf4ef25e2f2076b7ddbcff1ee158e3f03b2af6e
tatorres1 <tatorres1@espe.edu.ec>
2023-04-04 10:47:23 -0500

    Prueba uno

--- pages/app.js
+++ pages/app.js
@@ -1,6 +1,5 @@
 // pages/_app.js
-import './almacen/gestion_almacen.modulo.css'
-import './index.css'
+
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
