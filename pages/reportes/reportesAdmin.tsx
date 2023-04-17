cdd9f197604e20f75df4a78179ca19f3e77fc49c
hlsalazar <hlsalazar@espe.edu.ec>
2023-04-17 11:09:27 -0500

    creacion parcial de pdfs de reporte, falta datos de base de datos

--- pages/reportes/reportesAdmin.tsx
+++ pages/reportes/reportesAdmin.tsx
@@ -1,19 +1,66 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
+import { jsPDF } from "jspdf";
+import autoTable from 'jspdf-autotable';
 
 
 interface Data {
   tipo_reporte: string;
   fecha_reporte: string;
-
+  enlace: Function;
 }
 
 const Reportes_admin: React.FC = () => {
+
+  
+  const [proveedores, setProveedores] = useState([]);
+
+  async function getProveedores(){
+    const postData = {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/proveedores`,
+    postData);
+    const response = await res.json();
+    setProveedores(response.proveedores);
+  }
+
+  function crearReporteProveedores(){
+                  // Crear un nuevo objeto jsPDF
+          const doc = new jsPDF();
+
+          // Definir las opciones para la tabla
+          const options = {
+            fontSize: 12,
+            pageOrientation: 'landscape',
+            
+          };
+
+          // Crear el array de objetos que representan las filas de la tabla
+          const data = [
+            {id: 1, name: 'John Doe', age: 35},
+            {id: 2, name: 'Jane Smith', age: 28},
+            {id: 3, name: 'Bob Johnson', age: 42},
+          ];
+
+          // Llamar a la función autoTable para crear la tabla
+          autoTable(doc, {head:[['id', 'name', 'age']],},);
+          autoTable(doc, {body: data},);
+
+          // Guardar el documento con la tabla usando la función save
+          doc.save('tabla.pdf');
+
+  }
+
+
   const [data, setData] = useState<Data[]>([
-    { tipo_reporte: 'REPORTE VENTAS', fecha_reporte: '25-10-2023' },
-    { tipo_reporte: 'REPORTE PERSONAL', fecha_reporte: '25-10-2023' },
-    { tipo_reporte: 'REPORTE PROVEEDORES', fecha_reporte: '25-10-2023' },
-    { tipo_reporte: 'REPORTE ALMACEN', fecha_reporte: '25-10-2023' },
+    { tipo_reporte: 'REPORTE VENTAS', fecha_reporte: '25-10-2023' , enlace: crearReporteProveedores},
+    { tipo_reporte: 'REPORTE PERSONAL', fecha_reporte: '25-10-2023', enlace: crearReporteProveedores},
+    { tipo_reporte: 'REPORTE PROVEEDORES', fecha_reporte: '25-10-2023', enlace: crearReporteProveedores},
+    { tipo_reporte: 'REPORTE ALMACEN', fecha_reporte: '25-10-2023', enlace: crearReporteProveedores},
   ]);
   const router = useRouter();
 
@@ -29,7 +76,7 @@ const Reportes_admin: React.FC = () => {
       <div className='lg:flex lg:justify-end lg:object-right sm:justify-center sm:flex'>
         <img src={'../assets/images/logo.png'} alt="" />
       </div><br /><br />
-      <button type="button" className="ml-8 py-2.5 px-5 text-base font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">AGREGAR NUEVO</button>
+      <button onClick={getProveedores} type="button" className="ml-8 py-2.5 px-5 text-base font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">AGREGAR NUEVO</button>
       
       <div className='w-full p-4 relative overflow-x-auto sm:rounded-lg'>
         <table className='sm:rounded-lg w-full text-sm text-left text-gray-500 dark:text-gray-400'>
@@ -44,13 +91,14 @@ const Reportes_admin: React.FC = () => {
             {data.map(row => (
               <tr className='"bg-gray-800 border-b dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-600' key={row.tipo_reporte}>
                 <td className="border border-lime-900 px-6 py-4 text-center">
-                  <a href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">VER</a></td>
+                  <a onClick={row.enlace} href="#" className="font-medium text-blue-600 dark:text-blue-500 hover:underline">VER</a></td>
                 <td className='border border-lime-900 text-center text-lg text-green-500'>{row.tipo_reporte}</td>
                 <td className='border border-lime-900 text-center text-lg'>{row.fecha_reporte}</td>
               </tr>
             ))}
           </tbody>
-        </table>
+            </table>
+        
 
       </div>
 
