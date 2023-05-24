5852bd1ff4272a8bd1fc65708bfcce29eabde2a3
hlsalazar <hlsalazar@espe.edu.ec>
2023-05-24 08:31:32 -0500

    adicion comprador, completa parcial, posible cambio

--- pages/indexAdmin.tsx
+++ pages/indexAdmin.tsx
@@ -11,6 +11,8 @@ function App() {
     var direccion_proveedores = "./proveedores/gestionProveedores";
     var direccion_reportes = "./reportes/reportesAdmin";
     var direccion_facturacion = "./facturacion/indexFacturacion";
+    var direccion_comprador = "./comprador/gestionComprador";
+
 
     const router = useRouter();
     function SalirSesion() {
@@ -33,6 +35,9 @@ function App() {
     }
     function IrFacturacion() {
         router.push(direccion_facturacion);
+    }
+    function IrComprador() {
+        router.push(direccion_comprador);
     }
 
     return (
@@ -99,7 +104,14 @@ function App() {
                         </svg>
                     FACTURACION</button>
                 </div>
-
+                <div>
+                    <button type="button" className="flex items-center p-8 text-calc(4vw + 16px) font-bold bg-white hover:bg-gradient-to-bl focus:ring-4 focus:ring-green-200 rounded-lg text-center mr-2 mb-2"
+                        onClick={IrComprador}>
+                        <svg width="50%" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"></path>
+                        </svg>
+                    COMPRADORES</button>
+                </div>
 
             </div>
             
