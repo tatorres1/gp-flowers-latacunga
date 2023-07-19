5156d2d2c626827c1a7daaa4c10706821c6e9f48
Henry Salazar <hlsalazar@espe.edu.ec>
2023-07-18 22:38:29 -0500

    facturacion LISTA v2

--- pages/facturacion/gestionModificacionFacturacion.tsx
+++ pages/facturacion/gestionModificacionFacturacion.tsx
@@ -1398,7 +1398,7 @@ const Facturacion: React.FC = () => {
                 </div>
         </div>
         <div className='m-12'>
-                    <button onClick={() => {updateFacturacion()}}  class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-3xl font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
+                    <button onClick={() => {updateFacturacion(); router.back()}}  class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-3xl font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-500 to-pink-500 group-hover:from-purple-500 group-hover:to-pink-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-purple-200 dark:focus:ring-purple-800">
                       <span class="relative px-5 py-5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                           ACTUALIZAR
                       </span>
