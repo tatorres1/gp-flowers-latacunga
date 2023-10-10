0a564c3e85e0198263d467931b85c5757d1841de
hlsalazar <hlsalazar@espe.edu.ec>
2023-10-10 16:32:07 -0500

    adicion de funcionalidad gestion value cargo

--- pages/api/value_cargo.js
+++ pages/api/value_cargo.js
@@ -16,4 +16,72 @@ export default async function handler(req, res){
         res.status(200).json({cargo: cargo});
     }
 
+    if(req.method === "POST"){
+        const nombreComprador = req.body.nombre_comp;
+
+        const addCargo = await query({
+            query: "INSERT INTO valuecargo_facturacion (name) VALUES (?)",              
+            values: ([nombreComprador]),
+        });
+
+        if(addCargo.insertId){
+            message = "success";
+        } else {
+            message = "error";
+        }
+        let proveedor = {
+            //id_proveedor: addProveedor.insertId,
+            id_proveedor: addProveedor.insertId,
+            cedula_proveedor: cedulaProveedor,
+            nombre_proveedor: nombreProveedor,
+            telefono_proveedor: telefonoProveedor,
+            observaciones_proveedor: observacionesProveedor,  
+        };
+        res.status(200).json({response: {message: message, cargo: addCargo}});
+    }
+
+    if(req.method === "PUT"){
+
+        const idComprador = req.body.id_comp;
+        const nombreComprador = req.body.nombre_comp;
+
+        const updateCargo = await query({
+
+            query: "UPDATE valuecargo_facturacion SET name=? WHERE id=?",
+            values: ([nombreComprador, idComprador]),
+                         
+        });
+
+        //indicar varias constantes para la actualizacion de variables mediante query,
+        //primero revisar el tutorial para ver si realmente funciona
+        
+        const result = updateCargo.affectedRows;
+        if(result){
+            message = "success";
+        } else {
+            message = "error";
+        }
+        let cargo = {
+            id: idComprador,
+            name: nombreComprador,
+        };
+        res.status(200).json({response: {message: message, cargo: cargo}});
+    }
+    
+
+    if (req.method === "DELETE") {
+        const idComprador = req.body.id_comp;
+        const deleteCargo = await query({
+          query: "DELETE FROM valuecargo_facturacion WHERE id = ?",
+          values: [idComprador],
+        });
+        const result = deleteCargo.affectedRows;
+        if (result) {
+          message = "success";
+        } else {
+          message = "error";
+        }
+        res.status(200).json({ response: { message: message, cargo: cargo } });
+      }
+
 }
