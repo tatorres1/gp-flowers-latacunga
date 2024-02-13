903c2c3cfc9db12caa226796243d4a40fefc023e
Thalia <soporte4@iconta.ec>
2024-02-13 12:08:14 -0500

    Cmbio filtro

--- pages/api/flor_filtro.js
+++ pages/api/flor_filtro.js
@@ -1,19 +1,24 @@
 import { query } from "@/lib/database";
 
 export default async function handler(req, res) {
-
-
     const { id_gestionFlor, PROVEEDOR, VARIEDAD } = req.query;
 
     if (req.method === "GET") {
+        // Filtrar las condiciones que tienen valores no nulos
+        const conditions = [];
+        if (id_gestionFlor) conditions.push("id_gestionFlor=?");
+        if (PROVEEDOR) conditions.push("PROVEEDOR=?");
+        if (VARIEDAD) conditions.push("VARIEDAD=?");
 
-        const flor = await query(
-            {
-                query: "SELECT * FROM flor WHERE id_gestionFlor=? OR PROVEEDOR=? OR VARIEDAD=? OR =? ",
-                values: ([id_gestionFlor, PROVEEDOR, VARIEDAD]),
-            }
-        );
+        // Construir la consulta SQL con las condiciones
+        const whereClause = conditions.length > 0 ? `WHERE ${conditions.join(" OR ")}` : "";
+
+        // Ejecutar la consulta
+        const flor = await query({
+            query: `SELECT * FROM flor ${whereClause}`,
+            values: ([id_gestionFlor, PROVEEDOR, VARIEDAD]),
+        });
+
         res.status(200).json({ flor: flor });
     }
-
 }
