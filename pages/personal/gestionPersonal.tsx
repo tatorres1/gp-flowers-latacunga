c728e79e6e76ca1357d2b9ad8031d2c17936c679
hlsalazar <hlsalazar@espe.edu.ec>
2024-03-12 13:51:53 -0500

    adicion boton actualizar

--- pages/personal/gestionPersonal.tsx
+++ pages/personal/gestionPersonal.tsx
@@ -231,6 +231,18 @@ const Personal: React.FC = () => {
                     <br></br>
                 <button type="button" className="ml-8 py-2.5 px-5 mr-2 mb-2 text-sm font-medium focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                     onClick={() => setShowModal(true)}>AGREGAR NUEVO</button>
+
+                <button className='bg-blue-400 ml-12 py-1 px-4 mr-2 mb-2' onClick={AccionDesactivarFiltro}>
+                <div className='flex flex-row space-x-4'>
+                  <a>
+                    Actualizar
+                  </a>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-10 h-5">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                  </svg>
+                </div>
+                </button>
+
                 {created ? <div>Success!</div> :
                     null}
 
